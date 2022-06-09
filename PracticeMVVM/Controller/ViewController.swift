d7e354b892b6aaedc9369a4ed849ca8a044bcc7d
Zoe Liauh <woanjwuliauh@gmail.com>
2022-06-10 01:01:48 +0800

    refactor: move logic related code to viewModel from view

--- PracticeMVVM/Controller/ViewController.swift
+++ PracticeMVVM/Controller/ViewController.swift
@@ -13,15 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var listTableView: UITableView!
     
-    let service = RequestCommunicator<DownloadMusic>()
-    var musicHandlers: [MusicHandler] = []
-    let downloadImageQueue = OperationQueue()
+//    let service = RequestCommunicator<DownloadMusic>()
+//    var musicHandlers: [MusicHandler] = []
+//    let downloadImageQueue = OperationQueue()
+    
+    let viewModel = ViewControllerViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         initView()
+        bindViewModel()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -37,74 +40,97 @@ class ViewController: UIViewController {
         
     }
     
-    func prepareRequest(with name: String) {
-        service.request(type: .searchMusic(media: "music", entity: "song", term: name)) { [weak self] (result) in
-            switch result {
-            case .success(let response):
-                if let musicHandelr = MusicHandler.updateSearchResults(response.data, section: 0) {
-                    self?.musicHandlers.append(contentsOf: musicHandelr)
-                    self?.listTableView.reloadData()
-                }
-                
-            case .failure(let error):
-                print("Network error: \(error.localizedDescription)")
+    func bindViewModel() {
+        viewModel.onRequestEnd = { [weak self] in
+            DispatchQueue.main.async {
+                self?.listTableView.reloadData()
             }
         }
-        
     }
     
-    func getImage(string: String, at indexPath: IndexPath, cell: UITableViewCell) {
-        guard let url = URL(string: string) else { return }
-        downloadImageQueue.addOperation {
-           do {
-               let data = try Data(contentsOf: url)
-               let image = UIImage(data: data)
-               DispatchQueue.main.async {
-                    guard let listCell = cell as? ListCell else { return }
-                    listCell.albumImageView.image = image
-                    listCell.setNeedsLayout()
-               }
-               
-           } catch let error {
-               printLog(logs: [error.localizedDescription], title: "Get Image Error-")
-           }
-        }
-    }
+    
+//    func prepareRequest(with name: String) {
+//        service.request(type: .searchMusic(media: "music", entity: "song", term: name)) { [weak self] (result) in
+//            switch result {
+//            case .success(let response):
+//                if let musicHandelr = MusicHandler.updateSearchResults(response.data, section: 0) {
+//                    self?.musicHandlers.append(contentsOf: musicHandelr)
+//                    self?.listTableView.reloadData()
+//                }
+//
+//            case .failure(let error):
+//                print("Network error: \(error.localizedDescription)")
+//            }
+//        }
+//
+//    }
+    
+//    func getImage(string: String, at indexPath: IndexPath, cell: UITableViewCell) {
+//        guard let url = URL(string: string) else { return }
+//        downloadImageQueue.addOperation {
+//           do {
+//               let data = try Data(contentsOf: url)
+//               let image = UIImage(data: data)
+//               DispatchQueue.main.async {
+//                    guard let listCell = cell as? ListCell else { return }
+//                    listCell.albumImageView.image = image
+//                    listCell.setNeedsLayout()
+//               }
+//
+//           } catch let error {
+//               printLog(logs: [error.localizedDescription], title: "Get Image Error-")
+//           }
+//        }
+//    }
 
 }
 
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        prepareRequest(with: searchBar.text ?? "")
+        viewModel.searchText = searchBar.text ?? ""
         searchBar.endEditing(true)
+//        prepareRequest(with: searchBar.text ?? "")
+//        searchBar.endEditing(true)
     }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return musicHandlers.count
+        return viewModel.listCellViewModels.count
+//        return musicHandlers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath) as? ListCell else { return UITableViewCell() }
         
-        let handler = musicHandlers[indexPath.row]
-        cell.titleLabel.text = handler.collectionName
-        cell.descriptionLabel.text = handler.name
-        getImage(string: handler.imageUrl, at: indexPath, cell: cell)
+        let listCellViewModel = viewModel.listCellViewModels[indexPath.row]
+        cell.setup(viewModel: listCellViewModel)
+        
+//        let handler = musicHandlers[indexPath.row]
+//        cell.titleLabel.text = handler.collectionName
+//        cell.descriptionLabel.text = handler.name
+//        getImage(string: handler.imageUrl, at: indexPath, cell: cell)
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let handler = musicHandlers[indexPath.row]
         
-        convienceAlert(alert: "Tapped: \(handler.artist)",
-                alertMessage: "music: \(handler.name)",
+        let listCellViewModel = viewModel.listCellViewModels[indexPath.row]
+        
+        convienceAlert(alert: "Tapped: \(listCellViewModel.title)",
+                       alertMessage: "music: \(listCellViewModel.description)",
                      actions: ["確認"],
                   completion: nil, actionCompletion: nil)
+        
+//        let handler = musicHandlers[indexPath.row]
+//
+//        convienceAlert(alert: "Tapped: \(handler.artist)",
+//                alertMessage: "music: \(handler.name)",
+//                     actions: ["確認"],
+//                  completion: nil, actionCompletion: nil)
     }
     
 }
--- PracticeMVVM/View/ListCell.swift
+++ PracticeMVVM/View/ListCell.swift
@@ -15,6 +15,8 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    private var viewModel: ListCellViewModel?
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -26,10 +28,25 @@ class ListCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    // 將照片先清空或設成預設照片
     override func prepareForReuse() {
         super.prepareForReuse()
         albumImageView.image = nil
-        
+        self.viewModel?.onImageDownloaded = nil
     }
-
+    
+    // 將 viewModel 要顯示的資訊更新在 cell 上
+    func setup(viewModel: ListCellViewModel) {
+        self.viewModel = viewModel
+        
+        self.titleLabel.text = viewModel.title
+        self.descriptionLabel.text = viewModel.description
+        
+        self.viewModel?.onImageDownloaded = { [weak self] image in
+            DispatchQueue.main.async {
+                self?.albumImageView.image = image
+            }
+        }
+        self.viewModel?.getImage()
+    }
 }
